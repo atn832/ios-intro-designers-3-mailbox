34589c177d5bc545c637da6c18d81723572ae4d2
Anh Tuan Nguyen <anhtuan@yahoo-inc.com>
2014-09-23 18:10:32 -0700

    implemented restore original view on tap

--- Mailbox/ViewController.swift
+++ Mailbox/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var mail: UIImageView!
     var originalMailX: CGFloat!
     @IBOutlet weak var laterIcon: UIImageView!
+    @IBOutlet weak var listIcon: UIImageView!
     @IBOutlet weak var mailBackground: UIView!
     
     @IBOutlet weak var laterView: UIImageView!
     
+    @IBOutlet weak var mailsView: UIImageView!
     @IBOutlet weak var todoView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +32,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onActionViewTap(sender: UITapGestureRecognizer) {
+        let originY = self.mailsView.frame.origin.y
+        println("tap tap")
+        //fade out
+        //collapse mail
+        //show mail again
+        //expand mail
+        UIView.animateWithDuration(0.7, animations: { () -> Void in
+                //fade out
+                self.laterView.alpha = CGFloat(0)
+                self.todoView.alpha = CGFloat(0)
+            }, completion: { (flag: Bool) -> Void in
+                UIView.animateWithDuration(0.7, animations: { () -> Void in
+                    self.mailsView.frame.origin.y = self.mailsView.frame.origin.y - self.mail.frame.height
+                    }, completion: { (flag: Bool) -> Void in
+                        self.mail.frame.origin.x = CGFloat(0)
+                        UIView.animateWithDuration(0.7, animations: { () -> Void in
+                            self.mailsView.frame.origin.y = originY
+                            }, completion: { (flag: Bool) -> Void in
+                                println("done")
+                        })
+                })
+
+        })
+    }
+    
     @IBAction func onMessagePan(panGestureRecognizer: UIPanGestureRecognizer) {
         let break1 = CGFloat(-60.0)
         let break2 = CGFloat(-160.0)
@@ -47,18 +75,17 @@ class ViewController: UIViewController {
             
             let u = -Float(translation.x) / 60
             laterIcon.alpha = CGFloat(u)
-            // frictional drag
-//            if (mail.frame.maxX < maxX) {
-//                mail.frame.origin.x = maxX - mail.frame.width + (mail.frame.maxX - maxX) / CGFloat(10.0)
-//            }
             if (translation.x < break2) {
                 // brown background, list options
                 self.mailBackground.backgroundColor = UIColor(red: CGFloat(215/255.0), green: CGFloat(166/255.0), blue: CGFloat(120/255.0), alpha: CGFloat(1.0))
+                listIcon.hidden = false
+                laterIcon.hidden = true
             } else if (translation.x < break1) {
                 // yellow
                 self.mailBackground.backgroundColor = UIColor(red: CGFloat(249/255.0), green: CGFloat(209/255.0), blue: CGFloat(69/255.0), alpha: CGFloat(1.0))
+                listIcon.hidden = true
+                laterIcon.hidden = false
             }
-        
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
             println("Gesture ended at: \(point)")
             // decide to go up and down
